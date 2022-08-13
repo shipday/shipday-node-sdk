675dadc7bf8b5d9238aadfcf0b3c7dc1bf6bd223
kawnayeen <kawnayeen.08@gmail.com>
2022-08-13 19:49:09 +0600

    address validation added for pickup and dropoff address in order info request

--- integration/order/request/address.js
+++ integration/order/request/address.js
@@ -8,6 +8,19 @@ class Address {
     this.country = country;
   }
 
+  isValidAddress() {
+    if (typeof this.unit !== 'string')
+      throw new Error('unit need to be of string type');
+    if (typeof this.street !== 'string')
+      throw new Error('street need to be of string type');
+    if (typeof this.city !== 'string')
+      throw new Error('city need to be of string type');
+    if (typeof this.state !== 'string')
+      throw new Error('state need to be of string type');
+    if (typeof this.country !== 'string')
+      throw new Error('country need to be of string type');
+  }
+
   getRequestBody() {
     return {
       'address': {
