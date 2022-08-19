f8ff05bf6b57b7d7443e70dbb8fc8102f1b92191
kawnayeen <kawnayeen.08@gmail.com>
2022-08-19 22:03:33 +0600

    test added for on-demand delivery assign request

--- test/index.test.js
+++ test/index.test.js
@@ -4,6 +4,6 @@ const Shipday = require('../integration');
 describe('Shipday SDK', () => {
   it('should return version', () => {
     const shipday = new Shipday('api key');
-    assert.equal(shipday.sayHello(), 'shipday node sdk - v 1.0.0');
+    assert.equal(shipday.sayHello(), 'shipday node sdk - v 1.1.0');
   });
 });
---
+++ test/ondemand.delivery/assign.request.test.js
@@ -0,0 +1,63 @@
+const expect = require('chai').expect;
+const AssignRequest = require('../../integration/ondemand.delivery/assign.request');
+
+describe('Assign on demand delivery request', () => {
+
+  describe('name', () => {
+    it('should throw error if name is missing', () => {
+      const request = new AssignRequest(null, 754);
+      expect(request.isValidAssignRequest).to.throw();
+      expect(() => {
+        request.isValidAssignRequest()
+      }).to.throw('on demand delivery service name is null');
+    });
+    it('name should be string', () => {
+      const invalidRequest = new AssignRequest(5, 754);
+      expect(() => {
+        invalidRequest.isValidAssignRequest()
+      }).to.throw('on demand service name should be string');
+      const validRequest = new AssignRequest('SExpress', 754);
+      expect(() => validRequest.isValidAssignRequest()).to.not.throw();
+    });
+  });
+
+  describe('orderId', () => {
+    it('should throw error if orderId is missing', () => {
+      const request = new AssignRequest('SExpress', null);
+      expect(request.isValidAssignRequest).to.throw();
+      expect(() => {
+        request.isValidAssignRequest()
+      }).to.throw('order id is null');
+    });
+    it('orderId should be number', () => {
+      const invalidRequest = new AssignRequest('SExpress', '754');
+      expect(() => {
+        invalidRequest.isValidAssignRequest()
+      }).to.throw('orderId should be integer');
+      const validRequest = new AssignRequest('SExpress', 754);
+      expect(() => validRequest.isValidAssignRequest()).to.not.throw();
+    });
+  });
+
+  it('if tip exists, it should be number', () => {
+    const validRequest = new AssignRequest('SExpress', 754, 7);
+    expect(() => validRequest.isValidAssignRequest()).to.not.throw();
+
+
+    const invalidRequest = new AssignRequest('SExpress', 754, '7');
+    expect(() => {
+      invalidRequest.isValidAssignRequest()
+    }).to.throw('tip should be of number type');
+  });
+
+  it('if estimated reference exist, it should be string', () => {
+    const validRequest = new AssignRequest('SExpress', 754, null, 'es768');
+    expect(() => validRequest.isValidAssignRequest()).to.not.throw();
+
+
+    const invalidRequest = new AssignRequest('SExpress', 754, null, 76);
+    expect(() => {
+      invalidRequest.isValidAssignRequest()
+    }).to.throw('estimate reference should be of string type');
+  });
+});
