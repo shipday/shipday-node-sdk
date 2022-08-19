52428e3725f91988c8039aab83cf110c234aa969
kawnayeen <kawnayeen.08@gmail.com>
2022-08-19 22:03:33 +0600

    test added for on-demand delivery assign request

--- integration/ondemand.delivery/assign.request.js
+++ integration/ondemand.delivery/assign.request.js
@@ -9,15 +9,19 @@ class AssignRequest {
   isValidAssignRequest() {
     if (!this.name)
       throw new Error('on demand delivery service name is null');
+    if (typeof this.name !== 'string')
+      throw new Error('on demand service name should be string');
     if (!this.orderId)
       throw new Error('order id is null');
+    if (typeof this.orderId !== 'number')
+      throw new Error('orderId should be integer');
     if (this.tip) {
       if (typeof this.tip !== 'number')
         throw new Error('tip should be of number type');
     }
     if (this.estimateReference) {
       if (typeof this.estimateReference !== 'string')
-        throw new Error('estimate reference is should be of string type');
+        throw new Error('estimate reference should be of string type');
     }
   }
 
